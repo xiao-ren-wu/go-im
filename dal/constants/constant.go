949a98621d0c4f77ec965cdf1065b81705fd129f
yuwenbo-erik <yuwenbo.erik@gmail.com>
2023-05-29 22:43:04 +0800

    update

---
+++ dal/constants/constant.go
@@ -0,0 +1,12 @@
+package constants
+
+type OpCode int64
+
+const (
+	OpContinuation OpCode = 0x0
+	OpText         OpCode = 0x1
+	OpBinary       OpCode = 0x2
+	OpClose        OpCode = 0x8
+	OpPing         OpCode = 0x9
+	OpPong         OpCode = 0xa
+)
---
+++ dal/server.go
@@ -0,0 +1,44 @@
+package dal
+
+import (
+	"github.com/xiao-ren-wu/go-im/dal/constants"
+	"net"
+	"time"
+)
+
+type Server interface {
+}
+
+type Acceptor interface {
+	Accept(Conn, time.Duration) (string, error)
+}
+
+type Listener interface {
+	Disconnect(string) error
+}
+
+type Conn interface {
+	net.Conn
+	ReadFrame() (Frame, error)
+	WriteFrame(constants.OpCode, []byte) error
+	Flush() error
+}
+
+type ChannelMap interface {
+	Add(Channel)
+	Remove(id string)
+	Get(id string) (Channel, bool)
+	All() []Channel
+}
+
+type MessageListener interface {
+	ID() string
+	Push([]byte) error
+}
+
+type Frame interface {
+	SetOpCode(constants.OpCode)
+	GetOpCode() constants.OpCode
+	SetPayload(raw []byte)
+	GetPayload() []byte
+}
